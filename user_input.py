b441301c1bc9d1be7e1e7fca08ac88e1adbed13b
claire <clairemwas20@gmail.com>
2024-11-09 20:22:33 +0300

    program to input and dislay user info

---
+++ user_input.py
@@ -0,0 +1,7 @@
+
+
+
+name=input("What is your name? ")
+age=input("What is your age? ")
+location=input("What is your location? ")
+print(f"Hello {name}, you are {age} years old and you live in {location} ")
